7ac287f362315bb93540f6bd0e335fb656938348
zhiga <zhiger231002@gmail.com>
2024-06-05 03:18:43 +0600

    d

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,21 @@
-import app from './app';
+import express from 'express';
+import helmet from 'helmet';
+
+import api from './config/user-api';
+import cors from 'cors';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(helmet());
+app.use(cors({
+    origin: "*"
+}));
+app.use(express.json());
+
+app.use('/.netlify/functions/api', api);
+
 import serverless from "serverless-http";
 
 const port = process.env.PORT || 3005;
@@ -7,4 +24,3 @@ app.listen(port, () => {
 });
 
 export const handler = serverless(app);
-
