de0508a2ea40e466c140772c6b0f5834d73f980e
zhiga <zhiger231002@gmail.com>
2024-06-05 02:09:24 +0600

    init

---
+++ src/api/index.ts
@@ -0,0 +1,49 @@
+import express from 'express';
+import admin from "firebase-admin";
+import firebaseConfig from '../config/firebase.json';
+
+const router = express.Router();
+
+const app = admin.initializeApp({ credential: admin.credential.cert(firebaseConfig as any) });
+
+router.get<{}, any>('/user', async (req, res) => {
+  await app.auth().listUsers()
+    .then((listUsersResult) => {
+      const data = listUsersResult.users.map(user => ({
+        id: user.uid,
+        email: user.email,
+        createdAt: user.metadata.creationTime,
+        lastSignIn: user.metadata.lastSignInTime
+      }))
+      res.json(data);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        message: 'Some error',
+      });
+    });
+
+});
+
+router.delete('/user/:id', async (req, res) => {
+  const params = req.params as any;
+  const userId = params['id'];
+
+  await app.auth().deleteUser(userId)
+    .then((data) => {
+      res.json({
+        status: "success"
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.json({
+        message: 'Some error',
+      });
+    });
+
+});
+
+
+export default router;
---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import helmet from 'helmet';
+
+import api from './api';
+import cors from 'cors';
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(helmet());
+app.use(cors({
+    origin: "*"
+}));
+app.use(express.json());
+
+app.use('/api', api);
+
+export default app;
